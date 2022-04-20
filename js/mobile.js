86ea087ce7fdfdb39b7e3119a1ecf1995bfb7b9a
WebJubayergit config --global user.email <you@example.com>
2022-04-20 18:10:22 +0600

    js-half-done

--- js/mobile.js
+++ js/mobile.js
@@ -11,6 +11,47 @@ const loadData = () => {
         document.getElementById('empty-string').style.display = 'none';
         document.getElementById('error-input').style.display = 'none';
         const url = 'https://openapi.programming-hero.com/api/phones?search=${searchValue}'
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data.data.slice(0,20)))
+    }
+
+}
+
+
+
+// Display Result Function 
+const displayResult = (phones) => {
+    const result = document.getElementById('display-result');
+    result.textContent = '';
+    if (phones.length == 0){
+        document.getElementById('error-input').style.display = 'block';
+        document.getElementById('empty-string').style.display = 'none';
+    }
+    else{
+        document.getElementById('empty-string').style.display = 'none';
+        document.getElementById('error-input').style.display = 'none';
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML
+        })
 
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
